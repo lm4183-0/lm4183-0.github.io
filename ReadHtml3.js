363d3753874a15aceeb9501dff20b59aaf904bfe
lm4183-0 <121673639+lm4183-0@users.noreply.github.com>
2022-12-31 02:51:48 +0900

    Update ReadHtml3.js

--- ReadHtml3.js
+++ ReadHtml3.js
@@ -1,7 +1,2 @@
-<script type="text/javascript">
 document.getElementById("test1").innerHTML = "str1";
 document.getElementById("test4").innerHTML = str4;
-
-var obj = new XMLHttpRequest();
-var str = "";
-</script>
