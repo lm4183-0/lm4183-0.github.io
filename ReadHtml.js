d1620d9a2202efe99ae53690a92491f4ddaaac55
lm4183-0 <121673639+lm4183-0@users.noreply.github.com>
2022-12-31 01:54:54 +0900

    Update ReadHtml.js

--- ReadHtml.js
+++ ReadHtml.js
@@ -1,9 +1,9 @@
 var obj = new XMLHttpRequest();
 var str = obj.responseText;
-obj.open('GET','test.csv',true);
+obj.open('GET','https://lm4183-0.github.io/test.csv',true);
 obj.onreadystatechange = function(){
 	if (obj.readyState === 4 && obj.status === 200){
-		document.getElementById("https://lm4183-0.github.io/test").innerHTML = str;
+		document.getElementById("test").innerHTML = str;
 	}
 };
 
