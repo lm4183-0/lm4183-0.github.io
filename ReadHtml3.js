e1ab86b9a36c4f012c8646942984cfb4ba8062ef
lm4183-0 <121673639+lm4183-0@users.noreply.github.com>
2022-12-31 02:15:40 +0900

    Create ReadHtml3.js

---
+++ ReadHtml3.js
@@ -0,0 +1,11 @@
+var obj = new XMLHttpRequest();
+var str = "";
+
+obj.open('GET','./test.csv',true);
+obj.onreadystatechange = function(){
+	if (obj.readyState === 4 && obj.status === 200){
+		str = obj.responseText;
+		document.getElementById("test").innerHTML = "str3";
+	}
+};
+obj.send(null);
