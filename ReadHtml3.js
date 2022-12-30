5bd701f2ed1f049471e94cddd0a55f98eca167ec
lm4183-0 <121673639+lm4183-0@users.noreply.github.com>
2022-12-31 02:21:04 +0900

    Update ReadHtml3.js

--- ReadHtml3.js
+++ ReadHtml3.js
@@ -1,11 +1,7 @@
-var obj = new XMLHttpRequest();
-var str = "";
-
-obj.open('GET','./test.csv',true);
 obj.onreadystatechange = function(){
 	if (obj.readyState === 4 && obj.status === 200){
 		str = obj.responseText;
-		document.getElementById("test").innerHTML = "str3";
-	}
+		document.getElementById("test2").innerHTML = "str";
+		document.getElementById("test").innerHTML = str;
 };
 obj.send(null);
