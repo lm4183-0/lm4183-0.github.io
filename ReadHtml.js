d42a0d413c9d82a58447b7343e6e646c260efa77
lm4183-0 <121673639+lm4183-0@users.noreply.github.com>
2022-12-31 02:09:50 +0900

    Update ReadHtml.js

--- ReadHtml.js
+++ ReadHtml.js
@@ -1,7 +1,7 @@
 var obj = new XMLHttpRequest();
 var str = "";
 
-obj.open('GET','https://lm4183-0.github.io/test.csv',true);
+obj.open('GET','./test.csv',true);
 obj.onreadystatechange = function(){
 	if (obj.readyState === 4 && obj.status === 200){
 		str = obj.responseText;
