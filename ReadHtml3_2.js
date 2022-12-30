22faabea50fd5c988dc09585e3487921664f5fd8
lm4183-0 <121673639+lm4183-0@users.noreply.github.com>
2022-12-31 02:49:21 +0900

    Update ReadHtml3_2.js

--- ReadHtml3_2.js
+++ ReadHtml3_2.js
@@ -1,7 +1,3 @@
+<script type="text/javascript">
 document.getElementById("test1").innerHTML = "str1";
-document.getElementById("test4").innerHTML = str4;
-
-var obj = new XMLHttpRequest();
-var str = "";
-
-obj.open("GET","https://lm4183-0.github.io/test.csv",true);
+</script>
