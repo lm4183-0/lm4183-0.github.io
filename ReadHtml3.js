623a1c53e250e6d59411401cb8195e9c317f0c79
lm4183-0 <121673639+lm4183-0@users.noreply.github.com>
2022-12-31 03:02:55 +0900

    Update ReadHtml3.js

--- ReadHtml3.js
+++ ReadHtml3.js
@@ -1,4 +1,4 @@
-document.getElementById("test1").innerHTML = "str1";
+document.getElementById("test1").innerHTML = "str";
 
 var obj = new XMLHttpRequest();
 var str = "";
@@ -7,8 +7,7 @@ obj.open("GET","https://lm4183-0.github.io/test.csv",true);
 obj.onreadystatechange = function(){
 	if (obj.readyState === 4 && obj.status === 200){
 		str = obj.responseText;
-		document.getElementById("test2").innerHTML = "str2";
-		document.getElementById("test3").innerHTML = str;
+		document.getElementById("test1").innerHTML = str;
 	}
 };
 obj.send(null);
